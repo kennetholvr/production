0df4334c748c7a521ea571f030e5a319f2700381
Cornelius <bcornelius@iseatz.com>
2019-09-13 16:42:22 -0500

    feat: uses material components to style homepage

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
 
+export interface Tile {
+  cols: number;
+  rows: number;
+  text?: string;
+  color?: string;
+}
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
+
 export class HomepageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+    constructor() {}
+    ngOnInit() {}
+    tile: Tile[] = [{text: 'One', cols: 2, rows: 2}];
+    tiles: Tile[] = [
+      {text: 'Two', cols: 1, rows: 2, color: 'red'},
+      {text: 'Three', cols: 1, rows: 2, color: 'blue'},
+      {text: 'Four', cols: 1, rows: 2, color: 'green'},
+      {text: 'Five', cols: 1, rows: 2, color: 'yellow'},
+    ];
 
 }
