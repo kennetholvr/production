1b11d12b917877a0e99ab4cfe94dcda59fbd70e6
Cornelius <bcornelius@iseatz.com>
2019-09-16 09:54:52 -0500

    feat: dynamically adds styles

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -5,6 +5,7 @@ export interface Tile {
   rows: number;
   text?: string;
   color?: string;
+  id?: string;
 }
 @Component({
   selector: 'app-homepage',
@@ -15,12 +16,12 @@ export interface Tile {
 export class HomepageComponent implements OnInit {
     constructor() {}
     ngOnInit() {}
-    tile: Tile[] = [{text: 'One', cols: 2, rows: 2}];
     tiles: Tile[] = [
-      {text: 'Two', cols: 1, rows: 2, color: 'red'},
-      {text: 'Three', cols: 1, rows: 2, color: 'blue'},
-      {text: 'Four', cols: 1, rows: 2, color: 'green'},
-      {text: 'Five', cols: 1, rows: 2, color: 'yellow'},
+      {text: 'One', cols: 2, rows: 5, id: 'primaryCard'},
+      {text: 'Two', cols: 1, rows: 2, id: 'secondCard'},
+      {text: 'Three', cols: 1, rows: 2, id: 'thirdCard'},
+      {text: 'Four', cols: 1, rows: 2, id: 'fourthCard'},
+      {text: 'Five', cols: 1, rows: 2, id: 'fifthCard'},
     ];
 
 }
