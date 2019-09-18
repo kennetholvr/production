bc7b07ebe917c9eaa20dce94655ff5f33fb5074f
Cornelius <bcornelius@iseatz.com>
2019-09-18 10:12:36 -0500

    fix: merge

--- src/app/client-spec.ts
+++ src/app/client-spec.ts
@@ -13,10 +13,18 @@ const tiles: ITile[] = [
   {title: 'Four', cols: 2, rows: 3, id: 4, image: 'https://upload.wikimedia.org/wikipedia/en/5/53/Pickle_Rick.jpg'},
   {title: 'Five', cols: 2, rows: 3, id: 5, image: 'https://picsum.photos/200/300/?random'},
 ];
+const mobileTiles: ITile[] = [
+  {title: 'One', cols: 1, rows: 5, id: 1, image: '../../../assets/home.png'},
+  {title: 'Two', cols: 1, rows: 3, id: 2, image: '../../../assets/big.png'},
+  {title: 'Three', cols: 1, rows: 3, id: 3, image: '../../assets/little.png' },
+  {title: 'Four', cols: 1, rows: 3, id: 4, image: 'https://upload.wikimedia.org/wikipedia/en/5/53/Pickle_Rick.jpg'},
+  {title: 'Five', cols: 1, rows: 3, id: 5, image: 'https://picsum.photos/200/300/?random'},
+];
 const heading: string = 'Featured Projects'
 
 export {
   heading,
   ITile,
   tiles,
+  mobileTiles,
 }
